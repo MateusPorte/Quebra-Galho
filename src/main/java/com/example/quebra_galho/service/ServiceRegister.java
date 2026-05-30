29138081789e8e273df67b0e7e95eeba89756e85
Porte <mateusportesilva999@gmail.com>
2026-05-30 20:37:59 -0300

    fifth commit

--- src/main/java/com/example/quebra_galho/service/ServiceRegister.java
+++ src/main/java/com/example/quebra_galho/service/ServiceRegister.java
@@ -1,8 +1,23 @@
 package com.example.quebra_galho.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.example.quebra_galho.model.Register;
+import com.example.quebra_galho.repository.RepositoryRegister;
 
 @Service
 public class ServiceRegister {
+
+    private final RepositoryRegister repositoryRegister;
+
+    public ServiceRegister(RepositoryRegister repositoryRegister) {
+        this.repositoryRegister = repositoryRegister;
+    }
+
+    public List<Register> getAllRegisters() {
+        return repositoryRegister.findAll();
+    }
 
 }
